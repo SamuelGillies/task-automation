41c48bdb25a3193747e1570b45272cf9750a7f53
Sam Gillies <sam@samgillies.com>
2023-06-20 15:08:12 +0100

    fileRename formatted to rename Berk audio files for Heritage Quay

--- fileRename.py
+++ fileRename.py
@@ -1,32 +1,35 @@
 import os
 
-Source_Path = '/Users/samgillies/repos/task-automation/datain'
-Destination = '/Users/samgillies/repos/task-automation/dataout'
+Source_Path_MP3 = './MP3'
+Source_Path_WAV = './WAV'
+
+Destination_MP3 = './MP3'
+Destination_WAV = './WAV'
 
 
 def main():
-    for filename in os.listdir(Source_Path):
-        dst =  filename[0:3]    
+    for filename in os.listdir(Source_Path_MP3):
+            dst =  filename[0:3]
+
+            os.rename(os.path.join(Source_Path_MP3, filename),  os.path.join(Destination_MP3, dst + '.mp3'))
+            print(filename + ' renamed to ' + dst  + '.mp3')
+
+    for filename in os.listdir(Source_Path_WAV):
+        dst =  filename[0:3]
 
         if dst == '047': 
-            count = 0
-            count = count + 1
-            rename47 = '047' + '-' + count
-            os.rename(os.path.join(Source_Path, filename),  os.path.join(Destination, rename47))
-            print(filename + 'renamed to ' + rename47)
+            rename47 = '047' + '-' + filename[39:40]
+            os.rename(os.path.join(Source_Path_WAV, filename),  os.path.join(Destination_WAV, rename47 + '.wav'))
+            print(filename + ' renamed to ' + str(rename47) + '.wav')
         
-        elif dst == '181':
-            count = 0
-            count = count + 1
-            rename181 = '181' + '-' + count
-            os.rename(os.path.join(Source_Path, filename),  os.path.join(Destination, rename181)) 
-            print(filename + 'renamed to ' + rename181)
+        elif dst == '148':
+            rename148 = '148' + '-' + filename[19:20]
+            os.rename(os.path.join(Source_Path_WAV, filename),  os.path.join(Destination_WAV, rename148 + '.wav')) 
+            print(filename + ' renamed to ' + str(rename148) + '.wav')
 
         else:
-            os.rename(os.path.join(Source_Path, filename),  os.path.join(Destination, dst))
-            print(filename + 'renamed to ' + dst)
+            os.rename(os.path.join(Source_Path_WAV, filename),  os.path.join(Destination_WAV, dst + '.wav'))
+            print(filename + ' renamed to ' + dst + '.wav')
 
-
-# Driver Code
 if __name__ == '__main__':
     main()
