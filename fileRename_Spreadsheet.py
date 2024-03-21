1e2213b8b002530cd6485828973f19fcb7ff67c7
Sam Gillies <sam@samgillies.com>
2024-03-21 10:32:55 +0000

    toggleable suffix added

--- fileRename_Spreadsheet.py
+++ fileRename_Spreadsheet.py
@@ -3,6 +3,7 @@ import csv
 
 csv_file = './data/data.csv'
 source_directory = './source'
+input_suffix = '.JPG'
 destination_directory = './output'
 
 
@@ -16,18 +17,18 @@ def main():
         
         # Iterate through CSV rows
         for row in csvreader:
-            old_filename = os.path.join(source_directory, row[0] + '.JPG')
-            new_filename_single = os.path.join(destination_directory, row[1] + ', ' + row[2] + ' - ' + row[3] + '.JPG')
-            new_filename_multi = os.path.join(destination_directory, row[1] + ', ' + row[2] + ' - ' + row[3] + ', ' + row[4] + '.JPG')
+            old_filename = os.path.join(source_directory, row[0])
+            new_filename_single = os.path.join(destination_directory, row[1] + ', ' + row[2] + ' - ' + row[3] + input_suffix)
+            new_filename_multi = os.path.join(destination_directory, row[1] + ', ' + row[2] + ' - ' + row[3] + ', ' + row[4] + input_suffix)
 
             # Check if the file exists before renaming
             if os.path.exists(old_filename):
                 if row[4].strip(): 
                     os.rename(old_filename,  new_filename_multi)
-                    print(f'Renamed {row[0]} to {row[1] + ", " + row[2] + " - " + row[3] + ", " + row[4] + ".JPG"}')
+                    print(f'Renamed {row[0]} to {row[1] + ", " + row[2] + " - " + row[3] + ", " + row[4] + input_suffix}')
                 else: 
                     os.rename(old_filename,  new_filename_single)
-                    print(f'Renamed {row[0]} to {row[1] + ", " + row[2] + " - " + row[3] + ".JPG"}')
+                    print(f'Renamed {row[0]} to {row[1] + ", " + row[2] + " - " + row[3] + input_suffix}')
             else:
                 print(f'File not found: {row[0]}')
 
--- fileTranscriptionTranslation.py
+++ fileTranscriptionTranslation.py
@@ -1,5 +1,6 @@
 import os
-from PIL import Image
+from PIL import Image, ImageEnhance
+from langdetect import detect
 from natsort import natsorted
 import pytesseract
 import keys
@@ -8,41 +9,60 @@ import deepl
 source_directory = './source'
 destination_directory = './output'
 
+colour_enhancement_factor = 0.0    # black and white
+sharpness_enhancement_factor = 2.0      # sharpen image to max
+
 translator = deepl.Translator(keys.deepLKey)
 
 def main():
     for filename in natsorted(os.listdir(source_directory)):
         if not filename.startswith('.'):    # filter out .ds-store files
             file = source_directory + '/' + filename
-            fileOutputSingle = destination_directory + '/' + filename[:-5] + '.txt'
+            fileOutputSingle = destination_directory + '/' + filename[:-4] + '.txt'
             fileOutputMultiple = destination_directory + '/' + filename[:-7] + '.txt' # formatted for pages 1-9
             fileOutputMultiple2 = destination_directory + '/' + filename[:-8] + '.txt' # formatted for pages 10-99
 
-            transcription = pytesseract.image_to_string(Image.open(file), lang='deu')
-            translation = translator.translate_text(transcription, target_lang="EN-GB")
+            original_image = Image.open(file)
+            colour_enhance = ImageEnhance.Color(original_image)         # conversion to black and white and sharpen image to increase image clarity for OCR
+            black_white_image = colour_enhance.enhance(colour_enhancement_factor)
+            sharpness_enhance = ImageEnhance.Sharpness(black_white_image)
+            sharpest_image = sharpness_enhance.enhance(sharpness_enhancement_factor)
 
-            if file[-5].isdigit():
-                if file[-6].isdigit():
+            sharpest_image.save(destination_directory + '/test.jpg')
+
+            transcription = pytesseract.image_to_string(sharpest_image)
+            language = detect(transcription)
+
+            if language == 'en':        # logic to only use translation when the text is no english
+                # print('english')
+                translation = transcription
+            else:
+                # print('not english')
+                translation = translator.translate_text(transcription, target_lang="EN-GB")
+
+            if file[-5].isdigit():                          # formatted for Koln archive data, if last character is a digit true else false
+                if file[-6].isdigit():                      # if final number > 1 character then append text to existing document
                     # print(filename, 'page 10+')
                     with open(fileOutputMultiple2, 'a') as f: 
                         f.write(str(translation))
                         f.close
-                elif os.path.exists(fileOutputMultiple):
+                elif os.path.exists(fileOutputMultiple):    # if earlier page has been written, append text to existing document
                     # print(filename, 'file found')
                     with open(fileOutputMultiple, 'a') as f: 
                         f.write(str(translation))
                         f.close
-                else: 
+                else:                                       # create new document
                     # print(filename, 'new file created')
                     with open(fileOutputMultiple, 'w') as f: 
                         f.write(str(translation))
                         f.close
-            else: 
+            else:                                           # create new document as file is a single page
                 # print('new file created')
                 with open(fileOutputSingle, 'w') as f: 
                     f.write(str(translation))
                     f.close
 
+    print ('task complete')
 
 if __name__ == '__main__':
     main()
